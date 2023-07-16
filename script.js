2e2e4341e3d8b250ed49d9a2c6078eef2c2e63c8
malospal <79515861+MALSOAP123@users.noreply.github.com>
2023-07-17 01:22:58 +0600

    asd

--- script.js
+++
@@ -1,8 +0,0 @@
-let bg = document.querySelectorAll('body');
-for (let i = 0; i < bg.length; i++){
-    window.addEventListener('mousemove', function(e) { 
-        let x = e.clientX / window.innerWidth;
-        let y = e.clientY / window.innerHeight;     
-        bg[i].style.transform = 'translate(-' + x * 15 + 'px, -' + y * 15 + 'px)';
-    });    
-}   
